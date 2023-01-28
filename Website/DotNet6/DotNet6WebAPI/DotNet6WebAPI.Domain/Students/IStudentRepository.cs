b5bace6bbd75c6b8e237e726163e3b3a099fb2db
jone xu <670271826@qq.com>
2023-01-28 09:08:26 +0800

    Delete Website directory

--- Website/DotNet6/DotNet6WebAPI/DotNet6WebAPI.Domain/Students/IStudentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DotNet6WebAPI.Domain.Students.Entities;
-
-namespace DotNet6WebAPI.Domain.Students
-{
-    public interface IStudentRepository
-    {
-        Task<Student> Get(string id);
-    }
-}
--- Website/DotNetCore/CoreWebsite.Api/wwwroot/scripts/greeter.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (callback, name) {
-
-    var greet = function (name) {
-        return "Hello " + name;
-    }
-
-    callback(null, greet(name));
-}
